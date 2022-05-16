f6dc612dbf74f4dc6980fb27549572057bb4080a
Ricardo Pupo Larguesa <rlarguesa@gmail.com>
2022-05-16 15:55:39 -0300

    Aula 13 - Classe User

--- Aula13JdbcAcessControl/src/java/model/User.java
+++ Aula13JdbcAcessControl/src/java/model/User.java
@@ -16,6 +16,27 @@ public class User {
     private long passwordHash;
     private String name;
     
+    public static User getUser(String username, String password) throws Exception{
+        User user = null;
+        Class.forName("org.sqlite.JDBC");
+        String url = "jdbc:sqlite:mytasks.db";
+        Connection con = DriverManager.getConnection(url);
+        PreparedStatement stmt = con.prepareCall(
+                "select * from users where username = ? and pass_hash = ?");
+        stmt.setString(1, username);
+        stmt.setLong(2, (username+password).hashCode());
+        ResultSet rs = stmt.executeQuery();
+        if(rs.next()){
+            user = new User(
+                    rs.getString("username"), 
+                    rs.getLong("pass_hash"), 
+                    rs.getString("name")
+            );
+        }
+        rs.close(); stmt.close(); con.close();
+        return user;
+    }
+    
     public static ArrayList<User> getAllUsers() throws Exception{
         ArrayList<User> list = new ArrayList<>();
         Class.forName("org.sqlite.JDBC");
